b073203157bb7718f84fa27f52eb784d0fa2d0a0
Chris Blank <Christoph.Blank@kumavision.de>
2019-01-17 11:57:14 +0100

    Add Basic Apps

---
+++ AdditionalAppsViaChoco.ps1
@@ -0,0 +1,28 @@
+#Install Choco
+Log "Install Choco"
+Set-ExecutionPolicy Bypass -Scope Process -Force; iex ((New-Object System.Net.WebClient).DownloadString('https://chocolatey.org/install.ps1'))
+choco feature enable -n allowGlobalConfirmation
+choco install chocolatey
+
+Log "Install 7zip"
+choco install 7zip.install
+
+Log "Install notepadplusplus"
+choco install notepadplusplus.install --x86
+
+Log "Install multilingualapptoolkit"
+choco install multilingualapptoolkit
+
+Log "Install adobereader"
+choco install adobereader
+
+Log "Install firefox"
+choco install firefox
+
+Log "Install visualstudio2017community"
+choco install visualstudio2017community 
+
+Log "Install Chvisualstudio2017-workload-netweboco"
+choco install visualstudio2017-workload-netweb
+
+
