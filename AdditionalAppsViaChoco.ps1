3c68aebb14666b54867f6563608b67c4bb08fbe1
Chris Blank <Christoph.Blank@kumavision.de>
2019-01-17 14:22:25 +0100

    Add Microsoft.RdlcDesigner

--- AdditionalAppsViaChoco.ps1
+++ AdditionalAppsViaChoco.ps1
@@ -24,4 +24,9 @@ choco install visualstudio2017community
 Log "Install visualstudio2017-workload-netweb"
 choco install visualstudio2017-workload-netweb
 
-
+Log "Install Microsoft.RdlcDesigner"
+Import-Module BitsTransfer
+$Url = "https://probitools.gallerycdn.vsassets.io/extensions/probitools/microsoftrdlcreportdesignerforvisualstudio-18001/14.2/1517419538388/238792/3/Microsoft.RdlcDesigner.vsix"
+$target = "C:\DOWNLOAD\Microsoft.RdlcDesigner.vsix"
+Start-BitsTransfer -Source $url -Destination $target
+C:\Program Files (x86)\Microsoft Visual Studio 11.0\Common7\IDE\VSIXInstaller.exe /quiet $target
