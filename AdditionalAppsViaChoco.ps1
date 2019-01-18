10546521ff8287fb9ee87b34103300a0c97f9d7f
Chris Blank <Christoph.Blank@kumavision.de>
2019-01-18 17:05:31 +0100

    improve codestyle and reporting

--- AdditionalAppsViaChoco.ps1
+++ AdditionalAppsViaChoco.ps1
@@ -1,9 +1,9 @@
 #CopyBlock from https://github.com/Microsoft/nav-arm-templates/blob/master/Install-VS2017Community.ps1
 if (!(Test-Path function:Log)) {
-    function Log([string]$line, [string]$color = "Gray") {
-        ("<font color=""$color"">" + [DateTime]::Now.ToString([System.Globalization.DateTimeFormatInfo]::CurrentInfo.ShortTimePattern.replace(":mm",":mm:ss")) + " $line</font>") | Add-Content -Path "c:\demo\status.txt"
-        Write-Host -ForegroundColor $color $line 
-    }
+  function Log([string]$line, [string]$color = "Gray") {
+    ("<font color=""$color"">" + [DateTime]::Now.ToString([System.Globalization.DateTimeFormatInfo]::CurrentInfo.ShortTimePattern.replace(":mm",":mm:ss")) + " $line</font>") | Add-Content -Path "c:\demo\status.txt"
+    Write-Host -ForegroundColor $color $line 
+  }
 }
 #EndCopyBlock
 
@@ -33,11 +33,13 @@ choco install visualstudio2017community
 Log "Install visualstudio2017-workload-netweb"
 choco install visualstudio2017-workload-netweb
 
-Log "Install Microsoft.RdlcDesigner"
+Log "Download Microsoft.RdlcDesigner"
 $Url = "https://probitools.gallerycdn.vsassets.io/extensions/probitools/microsoftrdlcreportdesignerforvisualstudio-18001/14.2/1517419538388/238792/3/Microsoft.RdlcDesigner.vsix"
-$target = "C:\DOWNLOAD\Microsoft.RdlcDesigner.vsix"
-(New-Object System.Net.WebClient).DownloadFile($url, $target)
-$VsixInstaller = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\VSIXInstaller.exe"
-$Arguments = "/quiet $target"
-start-process $VsixInstaller $Arguments -Wait
-Log "nav-arm-extensions finished" -Color Green
+$Target = "C:\DOWNLOAD\Microsoft.RdlcDesigner.vsix"
+(New-Object -TypeName System.Net.WebClient).DownloadFile($Url, $Target)
+
+Log "Install Microsoft.RdlcDesigner"
+$VsixInstaller = "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\IDE\VSIXInstaller"
+$Arguments = "/quiet $Target"
+Start-Process -FilePath $VsixInstaller -ArgumentList $Arguments -Wait
+Log "nav-arm-extensions finished. MOre Infos at https://github.com/ChrisBlankDe/nav-arm-extensions" -Color Green
